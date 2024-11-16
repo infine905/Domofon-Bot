84ceed9dd49ea1d3daaf76b5951a3ae6990edab5
Light <78636670+infine905@users.noreply.github.com>
2024-11-16 18:39:31 +0300

    structs

--- utils/structs.py
+++ utils/structs.py
@@ -1,9 +1,16 @@
-
-
 class Tenant():
-    def __init__(self, id) -> None:
+    def __init__(self, id:int) -> None:
         self.id = id
+        
+class Apartament():
+    def __init__(self, id:int, name:str, address:str, tenants:list[Tenant]) -> None:
+        self.id = id
+        self.name = name
+        self.adress = address
+        self.tenants = tenants
 
-
-
-
+class Domofon():
+    def __init__(self, id, address:str, doors:list[tuple[str, int]]) -> None:
+        self.id = id
+        self.address = address
+        self.doors = doors
