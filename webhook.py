2f5aa45f1f0168fc9d5b812d83c751f31270e494
Light <78636670+infine905@users.noreply.github.com>
2024-11-17 11:22:34 +0300

    webhook

--- webhook.py
+++ webhook.py
@@ -5,17 +5,20 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def process_request():
     if request.method == 'GET':
-        domofon_id = request.args.get('domofon_id')
         tenant_id = request.args.get('tenant_id')
+        domofon_id = request.args.get('domofon_id')
+        apartment_id = request.args.get('apartment_id')
+        
     elif request.method == 'POST':
         data = request.json
-        domofon_id = data.get('domofon_id') if data else None
         tenant_id = data.get('tenant_id') if data else None
-
-    if not domofon_id or not tenant_id:
-        return jsonify({'message': 'Both domofon_id and tenant_id are required'}), 400
+        domofon_id = data.get('domofon_id') if data else None
+        apartment_id = data.get('apartment_id') if data else None
+        
+    if not domofon_id or not tenant_id or not apartment_id:
+        return jsonify({'message': 'Missing arguments, tenant_id, domofon_id and apartment_id are required'}), 400
     
     return jsonify({'message': 'Request processed successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, port=4123, host="0.0.0.0")
