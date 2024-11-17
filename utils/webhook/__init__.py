e818e55446adaaf67c974766e65b7c7d93f27ae3
Zhekop <cripper05@gmail.com>
2024-11-17 15:30:58 +0300

    init adn webhook

---
+++ utils/webhook/__init__.py
@@ -0,0 +1,2 @@
+from .webhook import core
+from .handler_webhook import webhookHandler
--- utils/webhook/handler_webhook.py
+++ utils/webhook/handler_webhook.py
@@ -1,7 +1,11 @@
 from aiogram import Bot
 from utils import Database
 
-async def webhookHandler(tenant_id, domofon_id, bot:Bot):
+from config import bot
+
+async def webhookHandler(tenant_id, domofon_id, apartment_id) -> bool:
+    
     chat_id=Database().GetOne(data='tg_id', table_name='Users', find_param='tenant_id', find_value=tenant_id)
     
     await bot.send_message(chat_id=chat_id, text='Вам позвонили')
+    print(1)
