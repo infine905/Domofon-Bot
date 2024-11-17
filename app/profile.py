32a93e910ef39bb18de9c6c4f48d02201f7039b7
Zhekop <cripper05@gmail.com>
2024-11-17 10:49:32 +0300

    123

--- app/profile.py
+++ app/profile.py
@@ -2,12 +2,22 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
-from utils import Database, generateKeyboard
+from requests import get, post
+import json
+
+from .register import sendContactFromUser
+from utils import Database, generateKeyboard, Domofon
 
 
 async def getProfile(message:Message, is_start:bool = False) -> None:
+    try:
+        tenant_id = Database().GetOne(data='tenant_id', table_name='Users', find_param='tg_id', find_value=message.from_user.id)
+    except:
+        await sendContactFromUser(message)
+        return
+    
     inline_keyboard = [
-        [InlineKeyboardButton(text='Мои квартиры', callback_data='get_apartment')],
+        [InlineKeyboardButton(text='Мои квартиры', callback_data=f'get_apartment_{tenant_id}')],
         [InlineKeyboardButton(text='Log Out', callback_data='logout')],
     ]
     keyboard = InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
@@ -19,11 +29,31 @@ async def getProfile(message:Message, is_start:bool = False) -> None:
     await message.edit_text(text='Меню', reply_markup=keyboard)
     
     
-def getDomofons(tenant_id) -> list[dict['domofon name':'domofon id']]:
-    '''
-    вернет список с доступными домофонами
-    '''
-    pass
+def getDofons(apartment_id:int, tenant_id:int) -> list[Domofon] | None:
+    url = f"https://domo-dev.profintel.ru/tg-bot/domo.apartment/{apartment_id}/domofon"
+    params  = {
+        "tenant_id" : tenant_id
+    }
+    headers = {
+        'x-api-key': 'SecretToken'
+    }
+
+    request = get(url=url, headers=headers, params=params )
+    print(request.status_code)
+    print(request.content.decode())
+    
+    if request.status_code != 200:
+        return None
+    
+    content = json.loads(request.content.decode())
+    
+    domofons = []
+    for item in content:
+        id = item['id']
+        name = item['name']
+        address = item['location']['readable_address']
+        print(id, name, address)
+        domofons.append(Domofon(id, name, address))
 
 
 # def a():
--- utils/structs.py
+++ utils/structs.py
@@ -3,25 +3,35 @@ class Tenant():
     def __init__(self, id:int) -> None:
         self._id = id
         
+    def __repr__(self):
+        return f"Tenant(id={self._id})"
+    
     def getId(self) -> int:
         return self._id
-        
-class Apartament():
+
+    
+class Apartment():
     _id = None
     _name = None
-    _adress = None
+    _address = None
     _tenants = None
     def __init__(self, id:int, name:str, address:str, tenants:list[Tenant]) -> None:
         self._id = id
         self._name = name
-        self._adress = address
+        self._address = address
         self._tenants = tenants
-
+        
+    def __repr__(self):
+        return f"Apartment(id={self._id}, name='{self._name}', address='{self._address}', tenants={self._tenants})"
+    
 class Domofon():
     _id = None
-    _doors = None
+    _name = None
     _address = None
-    def __init__(self, id, address:str, doors:list[tuple[str, int]]) -> None:
-        self.id = id
-        self.doors = doors
-        self.address = address
+    def __init__(self, id:int, name:str, address:str) -> None:
+        self._id = id
+        self._name = name
+        self._address = address
+        
+    def __repr__(self):
+        return f"Domofon(id={self._id}, name='{self._name}', address='{self._address}')" 
