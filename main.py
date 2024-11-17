b390116ae14f6652e96deb2c0a35ea5a7d8593b1
Zhekop <cripper05@gmail.com>
2024-11-17 16:21:07 +0300

    213

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from config.config import dp, bot
-from asyncio import run
+import asyncio 
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
 from time import strftime 
 
 import app
-from utils import set_command, core
+from utils import set_command, fastapi_app
 
-async def main():
+async def bot_main():
     dp.include_routers(
         app.RouterMain
     )
@@ -14,9 +16,17 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, skip_updates=True)
 
+async def fastapi_main():
+    config = Config()
+    config.bind = ["0.0.0.0:4123"] 
+    await serve(fastapi_app, config)
+
+async def main():
+    await asyncio.gather(bot_main(), fastapi_main())
+
+
 if __name__ == '__main__':
     now_time = strftime("%H:%M")
-    print(f'Bot started at {now_time}')
     
-    run(main()) 
-    core.run()
+    print(f'FastApi and Bot started at {now_time}')
+    asyncio.run(main())
